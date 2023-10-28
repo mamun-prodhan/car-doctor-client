20aa21b6a6e23e0d84b5e1b4a9a8f67116419267
mamun-prodhan <mamunpr9@gmail.com>
2023-10-28 13:47:03 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-5xl font-bold text-center">Hello world!</div>,
+  },
+]);
+
+export default router;
