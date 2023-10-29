949f36a05d5981e7fd10a990673b55a7c06ee126
mamun-prodhan <mamunpr9@gmail.com>
2023-10-30 01:25:14 +0600

    services section completed

---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,22 @@
+import { BsArrowRight } from "react-icons/bs";
+const ServiceCard = ({ service }) => {
+  const { title, img, price } = service;
+  return (
+    <div className="card bg-base-100 shadow-xl border">
+      <figure className="px-10 pt-10">
+        <img src={img} alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        <div className="flex items-center justify-between">
+          <p className="text-xl font-bold text-[#FF3811]">Price: ${price}</p>
+          <button className="btn text-[#FF3811] text-2xl font-bold">
+            <BsArrowRight></BsArrowRight>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
