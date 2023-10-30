79362981af66d359e4f1c87f7c897daf0f14bcac
mamun-prodhan <mamunpr9@gmail.com>
2023-10-30 15:23:55 +0600

    auth provider added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,10 +1,17 @@
-import { createContext } from "react";
-
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const authInfo = {
     name: "mamun Prodhan",
+    user,
+    loading,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
