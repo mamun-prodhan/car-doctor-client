5feb5850cb629c5b9167a7d823dd656f2c00f69b
mamun-prodhan <mamunpr9@gmail.com>
2023-10-31 11:35:02 +0600

    data get from server

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,6 +1,7 @@
 import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
 const ServiceCard = ({ service }) => {
-  const { title, img, price } = service;
+  const { _id, title, img, price } = service;
   return (
     <div className="card bg-base-100 shadow-xl border flex flex-col">
       <figure className="px-10 pt-10">
@@ -8,12 +9,14 @@ const ServiceCard = ({ service }) => {
       </figure>
       <div className="card-body">
         <h2 className="text-2xl font-bold">{title}</h2>
-        <div className="flex items-center justify-between">
-          <p className="text-xl font-bold text-[#FF3811]">Price: ${price}</p>
+        {/* <div className="flex items-center justify-between"> */}
+        <p className="text-xl font-bold text-[#FF3811]">Price: ${price}</p>
+        <Link to={`/checkout/${_id}`}>
           <button className="btn text-[#FF3811] text-2xl font-bold">
-            <BsArrowRight></BsArrowRight>
+            Book Now <BsArrowRight></BsArrowRight>
           </button>
-        </div>
+        </Link>
+        {/* </div> */}
       </div>
     </div>
   );
